3906d6fa1d4d9eb78d263a8a77019f9dfd065aa2
Josh Han <xhan91ca@gmail.com>
2018-08-15 14:08:11 -0700

    add index router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
+const index = path.join(__dirname, '../fe/dist/index.html');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(index);
 });
 
 module.exports = router;
